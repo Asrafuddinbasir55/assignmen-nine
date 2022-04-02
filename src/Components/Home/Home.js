dfe308f86746053783d3c5b9633e8169ee7f4bba
Asrafuddinbasir55 <asrafuddinbasir0@gamil.com>
2022-04-02 20:06:47 +0600

    update commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,19 +11,19 @@ import SingleReview from '../Reviews/SingleReview';
        
         return (
      <>
-           <div className='img-content'>
+           <div className='grid grid-cols-2 gap-8'>
 
              <div>
-                <h2 className='text-bold'>Your Best Camara</h2>
-                <h2>welcome to my webpage</h2>
+                <h2 className='p-2 text-xl font-bold'>Your Best Camara</h2>
+                <h2 className='p-2 text-xl font-bold'>welcome to my webpage</h2>
                 <p>Product design is the process designers use to blend user needs with business goals to help brands make consistently successful products. Product designers work to optimize the user experience in the solutions they make for their users—and help their brands by making products sustainable for longer-term business needs. </p>
 
-                <button className='bg-green-300 p-2 rounded text-right mt-8'>Live Demo</button>
+                <button className='bg-green-300 p-2 rounded text-right mt-8 text-2xl'>Live Demo</button>
                 
             </div>
 
             <div>
-           <h1>This is Camara</h1>
+           <h1 className='p-2 text-xl font-bold'>This is Camara</h1>
             <img src="https://i.ibb.co/F3WVtY0/camara.jpg"  alt="" />
           </div>
              
@@ -39,7 +39,7 @@ import SingleReview from '../Reviews/SingleReview';
               }
  
           </div>
-          <button className='bg-green-300 p-2 rounded text-right font-medium'>See All btn</button>
+          <button className='bg-green-300 p-2 rounded text-right font-normal text-2xl'>See All btn</button>
 
          </div> 
            
