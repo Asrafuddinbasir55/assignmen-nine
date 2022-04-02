02f55a44bef0dfdfc36ac237cba5ce5f66300360
Asrafuddinbasir55 <asrafuddinbasir0@gamil.com>
2022-04-02 19:47:07 +0600

    third commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,11 +14,11 @@ import SingleReview from '../Reviews/SingleReview';
            <div className='img-content'>
 
              <div>
-                <h2>Your Best Camara</h2>
+                <h2 className='text-bold'>Your Best Camara</h2>
                 <h2>welcome to my webpage</h2>
                 <p>Product design is the process designers use to blend user needs with business goals to help brands make consistently successful products. Product designers work to optimize the user experience in the solutions they make for their users—and help their brands by making products sustainable for longer-term business needs. </p>
 
-                <button className='btn-content'>Live Demo</button>
+                <button className='bg-green-300 p-2 rounded text-right mt-8'>Live Demo</button>
                 
             </div>
 
@@ -37,8 +37,9 @@ import SingleReview from '../Reviews/SingleReview';
               newReview?.map(rev => <SingleReview rev = {rev}
               key={rev.id}></SingleReview>)   
               }
+ 
           </div>
-          
+          <button className='bg-green-300 p-2 rounded text-right font-medium'>See All btn</button>
 
          </div> 
            
