2c05fe65e7cb31452f8227cc0da9f54fd5fddeaf
Asrafuddinbasir55 <asrafuddinbasir0@gamil.com>
2022-04-05 14:32:37 +0600

    change ornotfound filee commit

--- src/Components/NotFount/NotFount.js
+++ src/Components/NotFount/NotFount.js
@@ -3,7 +3,7 @@
      const NotFount = () => {
          return (
              <div>
-                 <h2>404, Not Found</h2>
+                 <h2 className='p-2 text-xl font-bold'>404, Not Found</h2>
              </div>
          );
      };
